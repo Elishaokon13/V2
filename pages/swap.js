dc2ea0e693f8227903c09c301b71d92a9f1d6b93
Elisha Okon <eliscodavid@gmail.com>
2023-03-10 20:59:14 +0000

    done final channges

--- pages/swap.js
+++ pages/swap.js
@@ -4,11 +4,11 @@ import { BridgesApp } from '@brgx/widget';   // import the widget
 
 export default function Swap() {
     return (
-        <div>
-            <Navbar />
-            <div className="justify-center items-center flex flex-col">
+        <section className="hero">
+            {/* <Navbar /> */}
+            <div className="justify-center items-center ml-20 flex flex-col">
             <BridgesApp />   // add the widget to the page
             </div>
-        </div>
+        </section>
     );
 }

540e38b576f735d2338e6864f3a44ad7f7cebcd3
Elisha Okon <eliscodavid@gmail.com>
2023-03-10 21:05:23 +0000

    swap.js

--- pages/swap.js
+++ pages/swap.js
@@ -1,14 +1,16 @@
 import { Navbar } from "../components";
 import { BridgesApp } from '@brgx/widget';   // import the widget
+import { Footer } from "../containers";
 
 
 export default function Swap() {
     return (
         <section className="hero">
-            {/* <Navbar /> */}
-            <div className="justify-center items-center ml-20 flex flex-col">
-            <BridgesApp />   // add the widget to the page
+            <Navbar />
+            <div className="justify-center items-center mt-20 ml-20 flex flex-col">
+            <BridgesApp /> 
             </div>
+            <Footer />
         </section>
     );
 }

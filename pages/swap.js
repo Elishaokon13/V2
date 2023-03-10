8f71fdeb76b68081844e75b1b6e328e2bc2ff6c3
Elisha Okon <eliscodavid@gmail.com>
2023-03-10 21:09:47 +0000

    lest's see if this works

--- pages/swap.js
+++ pages/swap.js
@@ -1,15 +1,20 @@
 import { Navbar } from "../components";
 import { BridgesApp } from '@brgx/widget';   // import the widget
 import { Footer } from "../containers";
+import { Blur, Navbar, Seo, Socials } from '../components';
 
 
 export default function Swap() {
     return (
         <section className="hero">
-            <Navbar />
+            <Seo />
+			<Eclipses />
+			<Navbar />
+			<Blur>
             <div className="justify-center items-center mt-20 ml-20 flex flex-col">
             <BridgesApp /> 
             </div>
+            </Blur>
             <Footer />
         </section>
     );

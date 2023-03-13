5f8c6c2dbfd8e12948c8913a7526de5fb66d03c1
Elisha Okon <eliscodavid@gmail.com>
2023-03-13 20:47:59 +0000

    swap

--- pages/swap.js
+++ pages/swap.js
@@ -4,6 +4,10 @@ export default function Swap() {
     return (
         <section className="hero">
             <div className="justify-center items-center mt-20 ml-20 flex flex-col">
+                <h1 className="text-4xl font-bold">Swap</h1>
+                <p className="text-2xl mt-4">Swap your tokens</p>
+            </div>
+            <div className="mt-20 ml-20">
             <BridgesApp />
             </div>
         </section>

5bb210f510fe9fc1d81490510d9734711d3e493f
Elisha Okon <eliscodavid@gmail.com>
2023-03-13 20:34:19 +0100

    Update swap.js

--- pages/swap.js
+++ pages/swap.js
@@ -1,15 +1,10 @@
 import { BridgesApp } from '@brgx/widget';
-import { Socials } from "../components";
-
-
 
 export default function Swap() {
     return (
         <section className="hero">
-            <Seo />
             <div className="justify-center items-center mt-20 ml-20 flex flex-col">
-            <BridgesApp /> 
-            <Socials isSlider={true} />
+            <BridgesApp />
             </div>
         </section>
     );

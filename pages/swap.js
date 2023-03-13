b9b824ac9b7a6fd2a85c8b8158c8b39db99f74bd
Elisha Okon <eliscodavid@gmail.com>
2023-03-13 21:12:20 +0000

    interfAce

--- pages/swap.js
+++ pages/swap.js
@@ -2,10 +2,9 @@ import { BridgesApp } from '@brgx/widget';
 
 export default function Swap() {
     return (
-        <section className="hero">
+        <section className="swap">
 
-            <h1 className="hero__title">Limitless Network Swap</h1>
-            <p className="hero__subtitle">Swap your tokens</p>
+            <h1 className="swap__title">Limitless Network Swap</h1>
             <div className="mt-20 ml-20 text-center justify-center">
                 <BridgesApp />
             </div>

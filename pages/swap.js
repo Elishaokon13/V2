86b88fd0f7b12b9651c2672c511a1c4f15531a26
Elisha Okon <eliscodavid@gmail.com>
2023-04-21 23:53:06 +0100

    Update swap.js

--- pages/swap.js
+++ pages/swap.js
@@ -8,7 +8,51 @@ export default function Swap() {
             <Header />
             <h1 className="swap__title">Limitless Network Swap</h1>
             <div className="mt-20 ml-20 text-center justify-center">
-                <PoodlApp />
+                <PoodlApp 
+                predefinedTokens={ [
+
+    {
+
+        address: "0xc13cbf50370e5eae6f5dd9d8a1015007f34c4ead",
+
+        chainId: 56,
+
+        decimals: 18,
+
+        hasTransactonFees: true,
+
+        logoURI: "",
+
+        name: "Limitless Network Token",
+
+        symbol: "LNT"
+
+    }
+
+  ]},
+
+defaultPinnedTokens={ {
+
+    56: [
+
+        "0xc13cbf50370e5eae6f5dd9d8a1015007f34c4ead"
+
+    ]
+
+  }},
+
+defaultSelectedTokenByChains={ {
+
+    56: {
+
+        defaultInputToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+
+        defaultOutputToken: "0xc13cbf50370e5eae6f5dd9d8a1015007f34c4ead"
+
+    }
+
+  }}
+                />
             </div>
         </section>
     );

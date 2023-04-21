b17f0e4889830cb7d7ab11ecf71ff7a25bc11cf9
Elisha Okon <eliscodavid@gmail.com>
2023-04-21 23:32:27 +0100

    done

--- pages/swap.js
+++ pages/swap.js
@@ -8,57 +8,7 @@ export default function Swap() {
             <Header />
             <h1 className="swap__title">Limitless Network Swap</h1>
             <div className="mt-20 ml-20 text-center justify-center">
-                <PoodlApp
-
-  predefinedTokens: [
-
-    {
-
-        address: "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD",
-
-        chainId: 56,
-
-        decimals: 18,
-
-        hasTransactonFees: true,
-
-        logoURI: "https://github.com/Elishaokon13/Limitless-Network-V2/blob/master/assets/images/Logo.png",
-
-        name: "Limitless Network Token",
-
-        symbol: "LNT"
-
-    }
-
-  ],
-
-defaultPinnedTokens: {
-
-    56: [
-
-        "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD"
-
-    ]
-
-  },
-
-defaultSelectedTokenByChains: {
-
-    56: {
-
-        defaultInputToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-
-        defaultOutputToken: "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD"
-
-    }
-
-  }
-
-
-
-  
-
-/>
+                <PoodlApp />
             </div>
         </section>
     );

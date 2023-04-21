82b83aa1cff25dc70bc0a7ad759a8ba5d09f26e3
Elisha Okon <eliscodavid@gmail.com>
2023-04-21 20:43:25 +0100

    Update swap.js
    Added CA

--- pages/swap.js
+++ pages/swap.js
@@ -8,7 +8,19 @@ export default function Swap() {
             <Header />
             <h1 className="swap__title">Limitless Network Swap</h1>
             <div className="mt-20 ml-20 text-center justify-center">
-                <PoodlApp />
+                <PoodlApp
+
+  defaultSelectedTokenByChains={{
+
+    56: {
+
+      defaultOutputToken: "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD"
+
+    }
+
+  }}
+
+/>
             </div>
         </section>
     );

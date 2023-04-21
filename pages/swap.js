49c7d4731b13664ce2cf40025527908d420285ca
Elisha Okon <eliscodavid@gmail.com>
2023-04-21 22:49:48 +0100

    Update swap.js
    SEO added to Contract address

--- pages/swap.js
+++ pages/swap.js
@@ -10,15 +10,53 @@ export default function Swap() {
             <div className="mt-20 ml-20 text-center justify-center">
                 <PoodlApp
 
-  defaultSelectedTokenByChains={{
+  predefinedTokens: [
 
-    56: {
+    {
 
-      defaultOutputToken: "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD"
+        address: "LNT_CONTRACT",
+
+        chainId: 56,
+
+        decimals: 18,
+
+        hasTransactonFees: true,
+
+        logoURI: "https://github.com/Elishaokon13/Limitless-Network-V2/blob/master/assets/images/Logo.png",
+
+        name: "Limitless Network Token",
+
+        symbol: "LNT"
 
     }
 
-  }}
+  ],
+
+defaultPinnedTokens: {
+
+    56: [
+
+        "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD"
+
+    ]
+
+  },
+
+defaultSelectedTokenByChains: {
+
+    56: {
+
+        defaultInputToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+
+        defaultOutputToken: "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD"
+
+    }
+
+  }
+
+
+
+  
 
 />
             </div>

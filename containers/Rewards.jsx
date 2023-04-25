4178ae3944d9bb783b3a68717cff17445f324629
Elisha Okon <eliscodavid@gmail.com>
2023-04-25 21:28:43 +0100

    Update Rewards.jsx
    Added value for rewards

--- containers/Rewards.jsx
+++ containers/Rewards.jsx
@@ -28,7 +28,7 @@ const Rewards = () => {
 						<span className='card__title'>Tier 1 Rewards</span>
 						<span className='card__content'>
 							<p>
-								We have a total of 3 reward tiers. The first tier is the most basic one, where you can earn rewards by simply holding LNT tokens in your wallet. The rewards are distributed daily and are based on the amount of LNT you hold.
+								We have a total of 3 reward tiers. The first tier is the most basic one, where you can earn 1% rewards by simply holding LNT tokens in your wallet. The rewards are distributed daily and are based on the amount of LNT you hold.
 							</p>
 						</span>
 						{/* <Link href='https://docs.kromatika.finance/'>
@@ -50,7 +50,7 @@ const Rewards = () => {
 						<span className='card__title'>Tier 2 Rewards</span>
 						<span className='card__content'>
 							<p>
-								The second tier is a bit more advanced. You can earn rewards by reinvesting your LNT tokens. This means that you can earn rewards by providing liquidity to the LNT/WBNB pool on our DEX. The rewards are distributed daily and are based on the amount of LNT you hold.
+								The second tier is a bit more advanced. You can earn rewards by reinvesting your LNT tokens. This means that you can earn 3% rewards by providing liquidity to the LNT/WBNB pool on our DEX. The rewards are distributed daily and are based on the amount of LNT you hold.
 							</p>
 						</span>
 						
@@ -65,7 +65,7 @@ const Rewards = () => {
 						<span className='card__title'>3rd Tier Rewards</span>
 						<span className='card__content'>
 							<p>
-								Hold on, we’re not done yet. The third tier is the most advanced one. You can earn rewards by holding 30,000,000 LNT tokens in your wallet. Reinvest dividends and earn rewards.
+								Hold on, we’re not done yet. The third tier is the most advanced one. You can earn 6% rewards by holding 30,000,000 LNT tokens in your wallet. Reinvest dividends and earn rewards.
 							</p>
 						</span>
 						

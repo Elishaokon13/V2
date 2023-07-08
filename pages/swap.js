6383797a7b85905cf395aa2d8d9d79f1c59fe775
DefiDevrel <58734811+Elishaokon@users.noreply.github.com>
2023-07-08 22:17:26 +0000

    swap update

--- pages/swap.js
+++ pages/swap.js
@@ -1,10 +1,12 @@
 import Head from "../components/Header"
+import { PoodlApp } from "@poodl/widget";
 
 export default function StakingPool() {
   return (
     <Head>
       <div className="container hero">
         <h1 className="hero__title">DEX Coming soon</h1>
+        <PoodlApp />
       </div>
     </Head>
   )

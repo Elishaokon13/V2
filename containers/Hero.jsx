04fb15eb34d8ebc3c8493c370fb75e8d3b6592e2
Elisha Okon <eliscodavid@gmail.com>
2023-08-04 23:08:54 +0100

    Update Hero.jsx

--- containers/Hero.jsx
+++ containers/Hero.jsx
@@ -7,10 +7,11 @@ const Hero = () => {
     <section id="section-hero" className="hero">
       <h1 className="hero__title">Limitless Network Finance</h1>
       <p className="hero__subtitle">
+      The Gold Standard of Crypto
       Enables easy, inclusive, and cost-effective DEX trading across different chains.
       </p>
       <div className="btn-group">
-        <Link passHref={true} href="/swap">
+        <Link passHref={true} href="https://app.limitlessnetwork.org/swap">
           <motion.a
             className="btn btn-solid"
             target="_blank"
@@ -21,15 +22,15 @@ const Hero = () => {
             Launch Dapp
           </motion.a>
         </Link>
-        {/* <Link passHref={true} href={whitepaper}>
+        <Link passHref={true} href="https://app.limitlessnetwork.org/">
           <motion.a
             className="btn btn-solid"
             whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
             whileTap={{ scale: 0.9 }}
           >
-            Whitepaper
+            Claim Rewards
           </motion.a>
-        </Link> */}
+        </Link>
       </div>
     </section>
   );
